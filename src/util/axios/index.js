6f4155e42956f0f044ad79fbf1eefa432dc2ad8d
somi <yj.somi@gmail.com>
2018-09-08 18:49:48 +0800

    update

--- src/util/axios/index.js
+++ src/util/axios/index.js
@@ -4,5 +4,16 @@ const instance = axios.create({
     baseURL: api_url,
     timeout: 3000
 })
-
+instance.interceptors.request.use(config=>{
+    return config
+},err=>{
+    return Promise.reject(err)
+})
+instance.interceptors.response.use(res=>{
+    console.log(res)
+    return res
+},err=>{
+    console.log(err.response)
+    return Promise.reject(err)
+})
 export default instance
